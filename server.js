963d8c5c9423a32ce71dbc5f34889b2a57574f9f
Ram4GB <leminhcuong2988@yahoo.com.vn>
2019-05-17 13:28:57 +0700

    fix bugs

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require("express");
+const port = process.env.PORT || 3000;
+var app = express();
+
+app.use(express.static(__dirname + "/dist/"));
+app.get(/.*/, function(req, res) {
+  res.sendFile(__dirname + "/dist/index.html");
+});
+
+app.listen(port);
